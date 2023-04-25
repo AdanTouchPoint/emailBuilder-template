9039b1c8ab3926e03a945859b66773b6c8ab8ef1
AdanTouchPoint <adan.mijangos@touchpointmarketing.mx>
2023-04-25 15:52:14 -0500

    fristCommit

---
+++ app/assets/petitions/fetchLeads.js
@@ -0,0 +1,24 @@
+import { fetchData } from "./fetchData";
+const fetchLeads = (
+  successResponse,
+  backendURLBase,
+  endpoints,
+  clientId,
+  dataUser,
+  emailData,questions
+) => {
+  fetchData(
+    "POST",
+    backendURLBase,
+    endpoints.toSaveLeads,
+    clientId,
+    `&firstName=${dataUser.userName ? dataUser.userName : ""}&postalcode=${
+      dataUser.zipCode ? dataUser.zipCode : dataUser.state
+    }&emailData=${dataUser.emailUser}&representative=${
+      emailData.name
+    }&emailMessage=${JSON.stringify(questions)}&sended=${successResponse}`
+  );
+  // axios.post(`https://payload-demo-tpm.herokuapp.com/leads?&firstName=${dataUser.userName ? dataUser.userName:''}&postalcode=${dataUser.zipCode ? dataUser.zipCode:dataUser.state}&emailData=${dataUser.emailUser}&representative=${emailData.name}&emailMessage=${dataUser.text}&city=${emailData.state}&party=${emailData.party}&clientId=${clientId}&sended=${respuestaDeExito}`, dataUseryEmail)
+};
+
+export { fetchLeads };
---
+++ app/assets/petitions/fetchRepresentatives.js
@@ -0,0 +1,19 @@
+import { fetchData } from "./fetchData";
+
+const fetchRepresentatives = async (petitionMethod, backendURLBase, endpoint, clientId, params = '', setMp, setSenator, setShowLoadSpin, setShowList) => {
+    const datos = await fetchData(petitionMethod, backendURLBase, endpoint, clientId, params)
+    let query = datos.data;
+    let fill = await query.map((el) => {
+      return el[0];
+    });
+    setMp(fill);
+    setSenator(datos.statesFilter)
+    setShowLoadSpin(false)
+    setShowList(false)
+
+}
+
+
+export{
+    fetchRepresentatives
+}
